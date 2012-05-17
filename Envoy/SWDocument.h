5692aa054a8980d04c826887e8ecb2a123582d79
Lee Walsh <Lee@ScribbleWare.local>
2012-05-17 12:21:51 +0100

    Initial Commit

---
+++ Envoy/SWDocument.h
@@ -0,0 +1,13 @@
+//
+//  SWDocument.h
+//  Envoy
+//
+//  Created by Lee Walsh on 17/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SWDocument : NSDocument
+
+@end
